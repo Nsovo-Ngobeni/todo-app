88b8b6dbcda25d379bb567b645fdead2cd9ce06d
Nsovo-Ngobeni <58889409+Nsovo-Ngobeni@users.noreply.github.com>
2020-12-20 17:10:00 +0200

    Project Innit
    Innitialised project using a Vue 3 template

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<h1>Hello World</h1>
+		<p> {{ msg }} </p>
+	</div>
+</template>
+
+<script>
+export default {
+	data: function() {
+		return {
+			msg: "Welcome to your new vue app"
+		}
+	}
+}
+</script>
+
+<style>
+	* {
+		background-color: #dedede;
+	}
+
+	div {
+		text-align: center;
+	}
+</style>
